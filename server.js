0e66f0985ce4bfd07e84dbd8ba2088ebca419684
David <dajohnson1998@gmail.com>
2018-12-17 15:25:25 -0500

    added express static server, and a start script

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 'use strict';
 
 // Load array of notes
+const express = require('express');
 const data = require('./db/notes');
+const app = express();
 
-console.log('Hello Noteful!');
+app.use(express.static("public"));
+
+app.listen(8080, function() {
+    console.info(`Server listening on ${this.address().port}`)
+}).on('error', err => {
+    console.error(err);
+})
+
+
 
 // INSERT EXPRESS APP CODE HERE...
