0058d71010cffeefa03c1f136bfc14a26ad0aff6
David <dajohnson1998@gmail.com>
2018-12-17 16:51:46 -0500

    added search functionality to the first app.get function

--- server.js
+++ server.js
@@ -9,19 +9,26 @@ app.use(express.static("public"));
 
 
 app.get('/api/notes', (req, res) => {
-    res.json(data);
-})
+  const searchTerm = req.query.searchTerm;
+  let returnItems = [];
+  for (let i=0; i < data.length; i++) {
+    if (data[i].title.includes(searchTerm)) {
+      returnItems.push(data[i])
+    }
+  }
+  res.json(returnItems);
+});
 
 app.get('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
-    let Item = data.find( item => item.id === Number(id));
-    res.json(Item);
+  const { id } = req.params;
+  let Item = data.find( item => item.id === Number(id));
+  res.json(Item);
 })
 
 app.listen(8080, function() {
-    console.info(`Server listening on ${this.address().port}`)
+  console.info(`Server listening on ${this.address().port}`)
 }).on('error', err => {
-    console.error(err);
+  console.error(err);
 })
 
 
